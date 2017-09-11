5438fd7ebf99245a39abc025214855bd2efdc0ec
Ivan Costa Lacuesta <icosta@tribeclick.com>
2017-09-11 22:19:53 +0200

    added dockerfile and test

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -1,6 +1,9 @@
 package consumer
 
 import (
+	"encoding/json"
+	"eventor/store"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -9,20 +12,35 @@ type entity struct {
 	id int
 }
 
+type updateListener struct {
+	Increment int
+}
+
 var _ = Describe("When I register a handler", func() {
+	var listener store.Listener
+	var consumer consumer
+
+	BeforeEach(func() {
+		listener = store.NewListener([]string{"localhost:9092"})
+		consumer = NewConsumer("business_entity", listener)
+	})
+
 	Context("And I call it", func() {
 		It("Should update the business entity", func() {
-			consumer := NewConsumer()
 			domainObject := entity{
 				id: 3,
 			}
-			consumer.Register("update listener", func() {
-				domainObject.id++
+			consumer.Register("update listener", func(b []byte) {
+				var payload updateListener
+				json.Unmarshal(b, &payload)
+
+				domainObject.id += payload.Increment
 			})
+			producerPayload := []byte(`{"increment":2}`)
 
-			consumer.fire("update listener")
+			consumer.fire("update listener", producerPayload)
 
-			Expect(domainObject.id).To(Equal(4))
+			Expect(domainObject.id).To(Equal(5))
 		})
 	})
 })
